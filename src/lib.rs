7b98d1efa2f2113d9b324c062370003d80acdb51
mochenya <2827075398@qq.com>
2023-11-02 09:53:07 +0800

    add init code.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
+mod tetris;
+
 use std::io;
 use std::io::Write;
 use clap::Parser;
-use crossterm::execute;
-use crossterm::terminal::DisableLineWrap;
+use crossterm::{execute, cursor};
+use crossterm::style::Color;
+use crossterm::terminal::ClearType;
 
 // cargo run -- -h
 #[derive(Parser, Debug)]
@@ -11,30 +14,34 @@ struct Arg {
     // development mode
     #[arg(short, long)]
     dev: bool,
-
 }
 
 pub fn run() {
     let arg = Arg::parse();
     println!("{:?}", arg);
-    execute!(io::stdout(), DisableLineWrap, crossterm::terminal::SetSize(20, 20)).unwrap();
     // 将从 0:0 开始到 20:20 的区域填充为白色
-    execute!(io::stdout(), crossterm::terminal::Clear(crossterm::terminal::ClearType::All)).unwrap();
+    execute!(io::stdout(), crossterm::terminal::Clear(ClearType::All)).unwrap();
     // 隐藏光标
-    execute!(io::stdout(), crossterm::cursor::Hide).unwrap();
+    execute!(io::stdout(), cursor::Hide).unwrap();
     // 输入背景色
     for index in 0..20 {
-        execute!(io::stdout(), crossterm::style::SetBackgroundColor(crossterm::style::Color::White)).unwrap();
         execute!(
             io::stdout(),
             crossterm::cursor::MoveTo(0, index),
-            Write
+            crossterm::style::SetBackgroundColor(Color::White)
         ).unwrap();
-        write!("                    ", "");
+        print!("　　　　　　　　　　　　　　　　　　　　");
+        execute!(io::stdout(), crossterm::style::SetBackgroundColor(Color::Black)).unwrap();
+        print!("|");
         // 打印空字符串
         io::stdout().flush().unwrap();
-        execute!(io::stdout(), crossterm::style::SetBackgroundColor(crossterm::style::Color::Black)).unwrap();
     }
+    execute!(
+            io::stdout(),
+            crossterm::cursor::MoveTo(0, 20)
+        ).unwrap();
+    print!("————————————————————————————————————————");
+    io::stdout().flush().unwrap();
     loop {
         // 等待输入q
         match crossterm::event::read().unwrap() {
@@ -46,4 +53,6 @@ pub fn run() {
             _ => {}
         }
     }
+    // 显示光标
+    execute!(io::stdout(), cursor::Show).unwrap();
 }
